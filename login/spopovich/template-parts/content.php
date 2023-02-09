6dd9959e1a02c1f0497486886b434c96a993b35b
Stephen Popovich <stephenpopovich16@gmail.com>
2023-02-09 10:49:14 -0800

    Remove Entry Title

--- login/spopovich/template-parts/content.php
+++ login/spopovich/template-parts/content.php
@@ -15,12 +15,6 @@
 	</div>
 	<header class="entry-header">
 		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php wp_bootstrap_starter_posted_on(); ?>
