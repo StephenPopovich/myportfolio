01bd7802cf9e8c1d7df17c7d6e198dafb205f962
Stephen Popovich <stephenpopovich16@gmail.com>
2023-02-25 15:11:22 -0800

    Attempt to fix the jQuery code

--- login/spopovich/js/custom.js
+++ login/spopovich/js/custom.js
@@ -142,37 +142,4 @@ document.addEventListener('mousemove', e => {
 	}
 })
 
-/*--------------------------------------------------------------
-# Counter Animator
---------------------------------------------------------------*/
-
-$(window).scroll(testScroll);
-var viewed = false;
-
-function isScrolledIntoView(elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
-
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
-
-function testScroll() {
-  if (isScrolledIntoView($(".counting")) && !viewed) {
-      viewed = true;
-      $('.count').each(function () {
-      $(this).prop('Counter',0).animate({
-          Counter: $(this).text()
-      }, {
-          duration: 4000,
-          easing: 'swing',
-          step: function (now) {
-              $(this).text(Math.ceil(now));
-          }
-      });
-    });
-  }
-}
 });
